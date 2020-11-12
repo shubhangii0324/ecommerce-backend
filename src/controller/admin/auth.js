dba9acd540eea5c1b3154300d4ec2452b181cdab
unknown <shubhangii0308@gmail.com>
2020-11-12 08:56:17 +0530

    ecommerce backend commit 1

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -1,6 +1,6 @@
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const shortid = require('shortid');
 
 exports.signup = (req, res) => {
