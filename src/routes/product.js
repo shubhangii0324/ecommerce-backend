733572e69f32b171e132e902149c4e6969a2e82e
unknown <shubhangii0308@gmail.com>
2020-12-17 21:10:25 +0530

    product delete route

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //const {  } = require('../controller/category');
 const { requireSignin, adminMiddleware, uploads3 } = require('../common-middleware');
-const { createProduct, getProductsBySlug, getProductDetailsById } = require('../controller/product');
+const { createProduct, getProductsBySlug, getProductDetailsById, deleteProductById, getProducts, } = require('../controller/product');
 const multer = require('multer');
 const router = express.Router();
 const shortid = require('shortid');
@@ -22,5 +22,18 @@ router.post('/product/create', requireSignin, adminMiddleware, uploads3.array('p
 router.get('/products/:slug', getProductsBySlug)
 //router.get('/category/getcategory', getCategories);
 router.get('/product/:productId', getProductDetailsById);
+
+router.delete(
+  "/product/deleteProductById",
+  requireSignin,
+  adminMiddleware,
+  deleteProductById
+);
+router.post(
+  "/product/getProducts",
+  requireSignin,
+  adminMiddleware,
+  getProducts
+);
 
 module.exports = router;
